6b1125dc2f5b523d34c16cc54b9079c545b08b00
sssiv <Sam.S.Skidmore@gmail.com>
2023-11-09 10:46:37 -0800

    Everything now has variable names and each variable name can only be used once

--- Code/State_Machine_Testing/generate.py
+++ Code/State_Machine_Testing/generate.py
@@ -6,90 +6,157 @@ def rand_num(x, y):
     return random.randint(x, y)
 
 # Makes a random choice from a list
-def rand_choise(i):
+def rand_choice(i):
     return random.choice(i)
+
+# Gets sublist index
+def get_list(sublist):
+
+    # Find index of list
+    index = tokens.lists.index(sublist)
+
+    # Gets list from index
+    list = tokens.lists[index]
+
+    # Returns found list
+    return list
 
 # Generates random proteus tokens
 def generate_random_text(list, length):
     random_text = ""
     line_length = 0
 
+    # These rows are for variable names, we want just random tokens
+    ignore_rows = -3
+
     # Random text still has more room
     while len(random_text) < length:
         # Picks randomly when to have a space, tab, or \n
         if line_length >= rand_num(1, 20):
             # Add a space, tab, or line break when the line length exceeds the random limit
-            random_text += rand_choise([" ", "\t", "\n"])
+            random_text += rand_choice([" ", "\t", "\n"])
 
             # Resets line counter
             line_length = 0
 
         # Starts making a new random line of code
         else:
-            # Randomly picks list, ignores the variable names 
-            selected_list = rand_choise(list[:-2])
+            # Randomly picks tokens, ignores the variable names 
+            selected_list = rand_choice(list[:ignore_rows])
 
             # Randomly picks item from list
-            selected_item = str(rand_choise(selected_list))  # Convert to string
+            selected_item = str(rand_choice(selected_list))  # Convert to string
 
             # Puts it in the random text
             random_text += selected_item
 
             # Updates the line length counter
             line_length += len(selected_item)
+            
     # Random code is now generated
     return random_text
 
+# Makes statemachine
 def generate_statemachine(code):
-    # Generate state machine
+    # List for used names to avoid redefining
+    used_names = []
     code += "    statemachine {\n"
 
-    # Pick random statename
-    state_name_index = tokens.lists.index(tokens.state_names)
-    statenames = tokens.lists[state_name_index]
-    statename = rand_choise(statenames)
+    # Pick random statename to have as initial
+    statename = get_list(tokens.state_names)
+    initial_statename = rand_choice(statename)
 
+    # Gets variable names list
+    var_names = get_list(tokens.variable_names)
+    
     # Randomly decides if we include an initial state 
     if rand_num(0, 1):
-        code += f"        initial {statename};\n\n"
+        code += f"        initial {initial_statename};\n\n"
 
     # Generate random variables
-    num_variables = random.randint(0, len(tokens.types))
-    for i in range(num_variables):
-        variable_type = random.choice(tokens.types)
-        variable_name = f"var_{i}"
-        code += f"        {variable_type} {variable_name};\n"
+    num_variables = rand_num(0, len(tokens.variable_names))
+    for _ in range(num_variables):
+        # Makes sure that randomly choses states are not already used
+        var_name = rand_choice(var_names)
+        while var_name in used_names:
+            var_name = rand_choice(var_names)
+
+        # Adds variable name in the used list
+        used_names.append(var_name)
+
+        # Random variable type is chosen
+        variable_type = rand_choice(tokens.variables)
+
+        # Variable syntax
+        code += f"        {variable_type} {var_name} = some value;\n"
+    
+    # Genrate States
     code += generate_state("")
+
+    # Return State machine syntax
     return code
 
+# Random states generated in statemachine
 def generate_state(code):
-    for i in range(rand_num(1, 10)):
-        code += f"\n        state State_{i} {{\n"
+    # Picks random statename
+    statenames = get_list(tokens.state_names)
+    statename = rand_choice(statenames)
+
+    # List for used names to avoid redefining
+    used_names = []
+
+    # Generates 0 - 50 states
+    for _ in range(rand_num(0, 50)):
+
+        # Makes sure that randomly choses states are not already used
+        while statename in used_names:
+            statename = rand_choice(statenames)
+        
+        # Adds used statename to list
+        used_names.append(statename)
+
+        # Actual State syntax
+        code += f"\n        state {statename} {{\n"
         code += "            // Add state behavior here\n"
         code += "        }\n"
     return code
 
 def generate_actor(code):
-    actor_name = rand_choise(tokens.lists[tokens.lists.index(tokens.actor_names)])
+    # Get actor names list
+    actor_names = get_list(tokens.actor_names)
+
+    # Picks random name from list
+    actor_name = rand_choice(actor_names)
+
     # Generate actor definition
     code = f"actor {actor_name} {{\n"
+
+    # Makes statmachine which calls generate_state
     code += generate_statemachine("")
     return code
+
 # Generate random code
 def generate_random_code():
-    # Select a random actor name
+
+    # Make actor, which calls statemachine which calls state
     code = generate_actor("")
-    # Close state machine and actor definitions
+
+    # Close state machine definitions
     code += "    }\n"
+
+    # Close actordefinitions
     code += "}\n"
 
+    # Returns random code
     return code
 
 # Show all tokens
-tokens.token_list()
-# Random token combo
-print(generate_random_text(tokens.lists, rand_num(1000, 2000)), '\n')
+tokens.display_tokens()
 
-# Generate and print random code
-random_code = generate_random_code()
-print(random_code, '\n')
+# Makes text file and writes random tokens in it
+with open('random_tokens.txt', 'w') as file:
+        file.write(generate_random_text(tokens.lists, rand_num(1000, 2000)))
+
+# Makes text file and writes random formatted code in it
+with open('random_code.txt', 'w') as file:
+        file.write(generate_random_code())
--- Code/State_Machine_Testing/tokens.py
+++ Code/State_Machine_Testing/tokens.py
@@ -15,16 +15,19 @@ keywords = [strip_single_quotes(entry) for entry in df['Keywords'].dropna().toli
 variables = [strip_single_quotes(entry) for entry in df['Variables'].dropna().tolist()]           # Variable Keywords (except variable_name at the end)
 operators = [strip_single_quotes(entry) for entry in df['Operators'].dropna().tolist()]           # All operators
 punctuation = [strip_single_quotes(entry) for entry in df['Punctuation'].dropna().tolist()]       # Any type of punctuation Proteus will allow
-actor_names = [strip_single_quotes(entry) for entry in df['Actor_Names'].dropna().tolist()] 
-state_names = [strip_single_quotes(entry) for entry in df['State_Names'].dropna().tolist()] 
+actor_names = [strip_single_quotes(entry) for entry in df['Actor_Names'].dropna().tolist()]       # Demo names for actors 
+state_names = [strip_single_quotes(entry) for entry in df['State_Names'].dropna().tolist()]       # Demo names for states
+variable_names = [strip_single_quotes(entry) for entry in df['Variable_Names'].dropna().tolist()]       # Demo names for variables
 
 # List of all proteus lists
-lists = [types, numbers, brackets, keywords, variables, operators, punctuation, actor_names, state_names]
-names = ['Types', 'Numbers', 'Brackets', 'Keywords', 'Variables', 'Operators', 'Punctuation', 'Actor_Names', 'State_Names']
+lists = [types, numbers, brackets, keywords, variables, operators, punctuation, actor_names, state_names, variable_names]
+names = ['Types', 'Numbers', 'Brackets', 'Keywords', 'Variables', 'Operators', 'Punctuation', 'Actor_Names', 'State_Names', 'Variable_Names']
 
 # Confirm everything has been collected
-def token_list():
+def display_tokens():
     print("-" * 100)
-    for name, lst in zip(names, lists):
-        print(f"{name}: {' '.join(map(str, lst))}")  # Convert numbers to strings for printing
+    # For every name in list
+    for name, list in zip(names, lists):
+        # Convert numbers to strings for printing
+        print(f"{name}: {' '.join(map(str, list))}\n")  
     print("-" * 100, "\n")
