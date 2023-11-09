faf4bc8baf837a23e8d2f27b22e6c733a8850a93
sssiv <Sam.S.Skidmore@gmail.com>
2023-11-09 12:06:17 -0800

    Needs work: Statemachine, generate_event. Make another py file cause it got messy

---
+++ Code/State_Machine_Testing/funcs.py
@@ -0,0 +1,42 @@
+import random
+import tokens
+
+# Generates a random number between x and y
+def rand_num(x, y):
+    return random.randint(x, y)
+
+# Makes a random choice from a list
+def rand_choice(options):
+    return random.choice(options)
+
+# Gets sublist index
+def get_list(sublist):
+
+    # Find index of list
+    index = tokens.lists.index(sublist)
+
+    # Gets list from index
+    list = tokens.lists[index]
+
+    # Returns found list
+    return list
+
+# Checks variable type and gives it a value
+def make_value(type):
+    # Bool check
+    if type == 'bool':
+        if rand_num(0, 1):
+            value = True
+        else:
+            value = False
+    
+    # Int check
+    if type == 'int':
+        value = rand_num(0, 100)
+
+    # String check
+    if type == 'string':
+        value = '"enter a string"'
+
+    # Returns value
+    return value
--- Code/State_Machine_Testing/generate.py
+++ Code/State_Machine_Testing/generate.py
@@ -1,45 +1,4 @@
-import random
-import tokens
-
-# Generates a random number from x to y
-def rand_num(x, y):
-    return random.randint(x, y)
-
-# Makes a random choice from a list
-def rand_choice(i):
-    return random.choice(i)
-
-# Gets sublist index
-def get_list(sublist):
-
-    # Find index of list
-    index = tokens.lists.index(sublist)
-
-    # Gets list from index
-    list = tokens.lists[index]
-
-    # Returns found list
-    return list
-
-# Checks variable type and gives it a value
-def make_value(type):
-    # Bool check
-    if type == 'bool':
-        if rand_num(0, 1):
-            value = True
-        else:
-            value = False
-    
-    # Int check
-    if type == 'int':
-        value = rand_num(0, 100)
-
-    # String check
-    if type == 'string':
-        value = '"enter a string"'
-
-    # Returns value
-    return value
+from funcs import *
 
 # Generates random proteus tokens
 def generate_random_text(list, length):
