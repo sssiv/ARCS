453a8e66175fad08d9277ce2cb5db566abdb7b51
sssiv <Sam.S.Skidmore@gmail.com>
2023-11-09 22:00:35 -0800

    Added an int main

--- Code/State_Machine_Testing/generate.py
+++ Code/State_Machine_Testing/generate.py
@@ -235,23 +235,3 @@ class Generate:
 
         # Returns random code
         return self.code
-
-# Show all tokens
-tokens.display_tokens()
-
-# Write tokens to a textfile
-tokens.tokens_txt()
-
-Generate = Generate()
-
-
-# Makes text file and writes random tokens in it
-num_of_chars = rand_num(1000, 2000)
-list_of_tokens = tokens.lists
-
-with open('random_tokens.txt', 'w') as file:
-        file.write(Generate.generate_random_text(list_of_tokens, num_of_chars))
-
-# Makes text file and writes random formatted code in it
-with open('random_code.txt', 'w') as file:
-    file.write(Generate.generate_random_code())
---
+++ Code/State_Machine_Testing/main.py
@@ -0,0 +1,21 @@
+from generate import *
+
+# Show all tokens
+tokens.display_tokens()
+
+# Write tokens to a textfile
+tokens.tokens_txt()
+
+# Object for making syntax
+Generate = Generate()
+
+# Makes text file and writes random tokens in it
+num_of_chars = rand_num(1000, 2000)
+list_of_tokens = tokens.lists
+
+with open('random_tokens.txt', 'w') as file:
+        file.write(Generate.generate_random_text(list_of_tokens, num_of_chars))
+
+# Makes text file and writes random formatted code in it
+with open('random_code.txt', 'w') as file:
+    file.write(Generate.generate_random_code())
