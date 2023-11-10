1e94174cfe953246538601ef8ccc5dbd640c2126
sssiv <Sam.S.Skidmore@gmail.com>
2023-11-09 22:23:30 -0800

    Quick Clean

--- Code/State_Machine_Testing/main.py
+++ Code/State_Machine_Testing/main.py
@@ -6,16 +6,15 @@ tokens.display_tokens()
 # Write tokens to a textfile
 tokens.tokens_txt()
 
-# Object for making syntax
-Generate = Generate()
-
 # Makes text file and writes random tokens in it
 num_of_chars = rand_num(500, 2000)
 list_of_tokens = tokens.lists
 
+# Not using an object for Generate() cause 
+# I want the generated data to be purposfully lost for new tests
 with open('random_tokens.txt', 'w') as file:
-        file.write(Generate.generate_random_text(list_of_tokens, num_of_chars))
+        file.write(Generate().generate_random_text(list_of_tokens, num_of_chars))
 
 # Makes text file and writes random formatted code in it
 with open('random_code.txt', 'w') as file:
-    file.write(Generate.generate_random_code())
+    file.write(Generate().generate_random_code())
